d5af83861a32a3754bba7e9ed85a6ab9d6759e8a
shashankroy573 <93342826+shashankroy573@users.noreply.github.com>
2022-09-23 18:02:30 +0530

    Added database files

---
+++ EmployeeAttendanceTrackingDatabase/EmployeeAttendanceTrackingDatabase/SchemaObjects/Schemas/dbo/Tables/EmployeeAttendanceDetails.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[EmployeeAttendanceDetails]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[EmployeeId] UNIQUEIDENTIFIER NOT NULL,
+	[LoginTime] DATETIME2 NOT NULL,
+	[LogoutTime] DATETIME2 NOT NULL, 
+    CONSTRAINT [PK_EmployeeAttendanceDetails] PRIMARY KEY ([Id]), 
+
+)
---
+++ EmployeeAttendanceTrackingDatabase/EmployeeAttendanceTrackingDatabase/SchemaObjects/Schemas/dbo/Tables/Employees.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employees]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL ,
+	[EmailId] NVARCHAR(255) NOT NULL,
+	[Password] NVARCHAR(1024) NOT NULL,
+	[CreatedTime] DATETIME2 NOT NULL,
+	[ModifiedTime] DATETIME2 NOT NULL, 
+    CONSTRAINT [PK_Employees] PRIMARY KEY ([Id]), 
+)
